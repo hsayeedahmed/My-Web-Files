d3cb4b544054f624772babcf125e8e2d9029e486
H. Sayeed Ahmed <sayeedahmedbsc@gmail.com>
2018-02-08 11:01:48 +0530

    Update AttendanceSystem.php

--- AttendanceSystem.php
+++ AttendanceSystem.php
@@ -20,12 +20,44 @@ while($rowval = mysqli_fetch_array($result))
 
  {
 
-$Regno= $rowval['Regno'];
+$Register_No= $rowval['Register_No'];
 
-$sname= $rowval['sname'];
+$sname= $rowval['Name'];
 
-$attendance= $rowval['attendance'];
-
+$OA_LAB= $rowval['OA_LAB'];
+	
+$PST1_LAB_LAB= $rowval['PST1_LAB'];
+	
+$C_LAB= $rowval['C_LAB'];
+	
+$PST2_LAB= $rowval['PST2_LAB'];
+	
+$CPPDS_LAB= $rowval['C++&DS_LAB'];
+	
+$INTERNET_LAB= $rowval['INTERNET_LAB'];
+	
+$AC1_LAB= $rowval['AC1_LAB'];
+	
+$JAVA_LAB= $rowval['JAVA_LAB'];
+	
+$PSA_LAB= $rowval['PSA_LAB'];
+	
+$AC2_LAB= $rowval['AC2_LAB'];
+	
+$MP_LAB= $rowval['MP_LAB'];
+	
+$MD_LAB= $rowval['MD_LAB'];
+	
+$CN_LAB= $rowval['CN_LAB'];
+	
+$CES_LAB= $rowval['CES_LAB'];
+	
+$DBMS_LAB= $rowval['DBMS_LAB'];
+	
+$OS_LAB= $rowval['OS_LAB'];
+	
+$OSP_LAB= $rowval['OSP_LAB'];
+	
 }
 
 mysqli_close($con);
@@ -93,15 +125,94 @@ mysqli_close($con);
 
         <tr>
 
-             <td style="color:red;background-color:aqua;" class="auto-style3">Attendance:</td>
+             <td style="color:red;background-color:aqua;" class="auto-style3">OA/OSW_LAB:</td>
 
             <td class="auto-style4">
 
-                <input id="Text3" type="text" value='<?php echo  $attendance; ?>' /></td>
+                <input id="Text3" type="text" value='<?php echo  $OA_LAB; ?>' /></td>
 
         </tr>
+       <tr>
 
-              
+             <td style="color:red;background-color:aqua;" class="auto-style3">PST1/CMP1_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $PST1_LAB; ?>' /></td>
+
+        </tr>
+       <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">Prog in C_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $C_LAB; ?>' /></td>
+
+        </tr>
+         <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">PST2/CMP2_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $PST2_LAB; ?>' /></td>
+
+        </tr>        
+       <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">C++ & DS_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $CPPDS_LAB; ?>' /></td>
+
+        </tr>
+	         <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">INTERNET_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $INTERNET_LAB; ?>' /></td>
+
+        </tr>
+	         <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">ACCOUNTANCY1_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $AC1_LAB; ?>' /></td>
+
+        </tr>
+	         <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">JAVA_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $JAVA_LAB; ?>' /></td>
+
+        </tr>
+	         <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">PSA_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $PSA_LAB; ?>' /></td>
+
+        </tr>
+	         <tr>
+
+             <td style="color:red;background-color:aqua;" class="auto-style3">ACCOUNTANCY2_LAB:</td>
+
+            <td class="auto-style4">
+
+                <input id="Text3" type="text" value='<?php echo  $AC2_LAB; ?>' /></td>
+
+        </tr>
     </table>
 
 </form>
